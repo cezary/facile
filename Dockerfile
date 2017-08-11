041eccc57823c46a382917cc46fa9fc6f9aff78c
Cezary Wojtkowski <cezary@gmail.com>
2017-08-11 10:48:50 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+# This is a sample Dockerfile you can modify to deploy your own app based on face_recognition
+
+FROM python:3.4-slim
+
+RUN apt-get -y update
+RUN apt-get install -y --fix-missing \
+    build-essential \
+    cmake \
+    gfortran \
+    git \
+    wget \
+    curl \
+    graphicsmagick \
+    libgraphicsmagick1-dev \
+    libatlas-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libboost-all-dev \
+    libgtk2.0-dev \
+    libjpeg-dev \
+    liblapack-dev \
+    libswscale-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    software-properties-common \
+    zip \
+    && apt-get clean && rm -rf /tmp/* /var/tmp/*
+
+RUN cd ~ && \
+    mkdir -p dlib && \
+    git clone -b 'v19.4' --single-branch https://github.com/davisking/dlib.git dlib/ && \
+    cd  dlib/ && \
+    python3 setup.py install --yes USE_AVX_INSTRUCTIONS
+
+
+# The rest of this file just runs an example script.
+
+WORKDIR /app
+
+# If you wanted to use this Dockerfile to run your own app instead, maybe you would do this:
+ADD . /app
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python3", "web_service.py"]
---
+++ web_service.py
@@ -0,0 +1,109 @@
+# This is a _very simple_ example of a web service that recognizes faces in uploaded images.
+# Upload an image file and it will check if the image contains a picture of Barack Obama.
+# The result is returned as json. For example:
+#
+# $ curl -F "file=@obama2.jpg" http://127.0.0.1:5001
+#
+# Returns:
+#
+# {
+#  "face_found_in_image": true,
+#  "is_picture_of_obama": true
+# }
+#
+# This example is based on the Flask file upload example: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+# $ curl curl http://localhost:5001\?url\=https://scontent-lax3-1.cdninstagram.com/t51.2885-15/s320x320/e35/20687008_1639332866078302_2901096829007429632_n.jpg
+#
+# Returns:
+#
+# {
+#   "count": 1,
+#   "faces": [
+#     {
+#       "bottom": 138,
+#       "left": 104,
+#       "right": 179,
+#       "top": 63
+#     }
+#   ],
+#   "found": true,
+#   "image": {
+#     "height": 320,
+#     "width": 320
+#   }
+# }
+
+import face_recognition
+from flask import Flask, jsonify, request, redirect
+import requests;
+from PIL import Image;
+
+# You can change this to any folder on your system
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__, static_url_path='')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        print(file)
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # The image file seems valid! Detect faces and return the result.
+            return detect_faces_in_image(file)
+
+    if 'url' in request.args:
+        url = request.args['url']
+        response = requests.get(url, stream=True);
+        return detect_faces_in_image(response.raw)
+
+    return app.send_static_file('index.html')
+
+def map_face_location(face_location):
+    top, right, bottom, left = face_location;
+    return {'top': top, 'right': right, 'bottom': bottom, 'left': left};
+
+def detect_faces_in_image(file_stream):
+    # Load the uploaded image file
+    img = face_recognition.load_image_file(file_stream)
+    # Get face encodings for any faces in the uploaded image
+    face_locations = face_recognition.face_locations(img)
+
+    pil_im = Image.fromarray(img)
+    height, width = pil_im.size
+
+    face_found = len(face_locations)
+
+    if len(face_locations) > 0:
+        face_found = True
+
+    face_count = len(face_locations)
+
+    face_locations = list(map(map_face_location, face_locations))
+
+    # Return the result as json
+    result = {
+        "count": face_count,
+        "found": face_found,
+        "faces": face_locations,
+        "image": { 'height': height, 'width': width }
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
