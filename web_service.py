7efedd2634e8b31490acf7a18b0f1eddd52ba4d1
Cezary Wojtkowski <cezary@gmail.com>
2017-08-11 11:15:44 -0700

    adds rate limiter

--- web_service.py
+++ web_service.py
@@ -35,44 +35,27 @@
 # }
 
 import face_recognition
-from flask import Flask, jsonify, request, redirect
-import requests;
 from PIL import Image;
+from flask import Flask, jsonify, make_response, request, redirect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import requests;
 
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__, static_url_path='')
 
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=['200 per day'],
+    headers_enabled=True
+)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_image():
-    # Check if a valid image file was uploaded
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-
-        file = request.files['file']
-        print(file)
-
-        if file.filename == '':
-            return redirect(request.url)
-
-        if file and allowed_file(file.filename):
-            # The image file seems valid! Detect faces and return the result.
-            return detect_faces_in_image(file)
-
-    if 'url' in request.args:
-        url = request.args['url']
-        response = requests.get(url, stream=True);
-        return detect_faces_in_image(response.raw)
-
-    return app.send_static_file('index.html')
 
 def map_face_location(face_location):
     top, right, bottom, left = face_location;
@@ -104,6 +87,41 @@ def detect_faces_in_image(file_stream):
         "image": { 'height': height, 'width': width }
     }
     return jsonify(result)
+
+@app.route('/', methods=['GET'])
+@limiter.exempt
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/v1/faces', methods=['GET', 'POST'])
+@limiter.limit('100 per day')
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        print(file)
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # The image file seems valid! Detect faces and return the result.
+            return detect_faces_in_image(file)
+
+    if 'url' in request.args:
+        url = request.args['url']
+        response = requests.get(url, stream=True);
+        return detect_faces_in_image(response.raw)
+
+@app.errorhandler(429)
+def rate_limit_handler(e):
+    return make_response(
+            jsonify(error="ratelimit exceeded %s" % e.description)
+            , 429
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
